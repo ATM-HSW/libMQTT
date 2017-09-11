230854795d4bce3494fda40b027da8f10239befa
Jan Jongboom <janjongboom@gmail.com>
2017-09-11 16:43:10 +0200

    Roll back f313853d22a6 and 08571008b958 - Usage of free'd memory. Needs investigation

--- MQTTmbed.h
+++ MQTTmbed.h
@@ -8,46 +8,42 @@ class Countdown
 public:
     Countdown()
     {
-        t = new Timer();
+        t = Timer();   
     }
-
+    
     Countdown(int ms)
     {
-        t = new Timer();
-        countdown_ms(ms);
+        t = Timer();
+        countdown_ms(ms);   
     }
-
-    ~Countdown()
-    {
-        delete t;
-    }
-
+    
+    
     bool expired()
     {
-        return t->read_ms() >= interval_end_ms;
+        return t.read_ms() >= interval_end_ms;
     }
-
-    void countdown_ms(unsigned long ms)
+    
+    void countdown_ms(unsigned long ms)  
     {
-        t->stop();
+        t.stop();
         interval_end_ms = ms;
-        t->reset();
-        t->start();
+        t.reset();
+        t.start();
     }
-
+    
     void countdown(int seconds)
     {
         countdown_ms((unsigned long)seconds * 1000L);
     }
-
+    
     int left_ms()
     {
-        return interval_end_ms - t->read_ms();
+        return interval_end_ms - t.read_ms();
     }
-
+    
 private:
-    Timer *t;
-    unsigned long interval_end_ms;
+    Timer t;
+    unsigned long interval_end_ms; 
 };
 
 #endif
